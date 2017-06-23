fa08801e0669b4bc3400c106ed21fc40494e4e9e
liuchenxi <lcx010222@163.com>
2017-06-23 16:26:14 +0800

    123

--- api/controllers/ExchangeController.js
+++ api/controllers/ExchangeController.js
@@ -8,18 +8,17 @@
 var http = require('http');
 var CryptoJS = require("crypto-js");
 var querystring = require('querystring');
-// var key = '5Df8$&@S';
-// var iv = CryptoJS.enc.Utf8.parse(key);
-// var key = CryptoJS.enc.Utf8.parse(key);
+var key = '5Df8$&@S';
+var iv = CryptoJS.enc.Utf8.parse(key);
+var key = CryptoJS.enc.Utf8.parse(key);
 module.exports = {
   getUserVipInfo: function(request, response, callback) {
     var data = querystring.stringify({
-      token: request.body.token
+      token: this.des(request.body.token)
     });
     var options = {
-      hostname: '121.40.211.34',
-      port: 8089,
-      path: '/stone-rest/rest/user/getUserVipInfo.json',
+      hostname: 'api.ppmiao.com',
+      path: '/user/getUserVipInfo.json',
       method: 'POST',
       agent: false,
       headers: {
@@ -40,7 +39,38 @@ module.exports = {
         clearTimeout(responseTimer);
         if (res.statusCode == 200) {
           let resp = JSON.parse(body);
-          response.send(JSON.parse(resp.resText));
+          var key = '5Df8$&@S';
+          var iv = CryptoJS.enc.Utf8.parse(key);
+          var key = CryptoJS.enc.Utf8.parse(key);
+          var decrypted = CryptoJS.TripleDES.decrypt({
+              ciphertext: CryptoJS.enc.Base64.parse(resp.resText)
+          }, key, {
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+          });
+          console.log(decrypted);
+          console.log("------------解码后-----------------");
+          var decrypt = decrypted.toString(CryptoJS.enc.Utf8);
+          console.log(decrypt);
+
+          var data = [{
+            id: 1
+          }, {
+            id: 2
+          }]
+          // Encrypt
+          var ciphertext = CryptoJS.TripleDES.encrypt(JSON.stringify(data), 'secret key 123');
+
+          // Decrypt
+          var bytes = CryptoJS.TripleDES.decrypt(ciphertext.toString(), 'secret key 123');
+          console.log(bytes);
+          var decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+          console.log(decryptedData);
+
+
+
+          response.send(decrypt);
         }
       });
     });
@@ -55,7 +85,7 @@ module.exports = {
   },
   getExchangeList: function(request, response, callback) {
     var data = querystring.stringify({
-      token: request.body.token
+      token: this.des(request.body.token)
     });
 
     var options = {
@@ -97,7 +127,7 @@ module.exports = {
   },
   claimExchange: function(request, response, callback) {
     var data = querystring.stringify({
-      token: request.body.token,
+      token: this.des(request.body.token),
       id: request.body.id
     });
     var options = {
@@ -139,7 +169,7 @@ module.exports = {
   },
   getWeeklyAward: function(request, response, callback) {
     var data = querystring.stringify({
-      token: request.body.token
+      token: this.des(request.body.token)
     });
 
     var options = {
