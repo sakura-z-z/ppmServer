f12aefe0598342a84cf743feea9f66afe6a61976
liuchenxi <lcx010222@163.com>
2017-06-23 11:49:57 +0800

    去掉密钥

--- api/controllers/ExchangeController.js
+++ api/controllers/ExchangeController.js
@@ -17,8 +17,9 @@ module.exports = {
       token: this.des(request.body.token)
     });
     var options = {
-      hostname: 'api.ppmiao.com',
-      path: '/user/getUserVipInfo.json',
+      hostname: '121.40.211.34',
+      port: 8089,
+      path: '/stone-rest/rest/user/getUserVipInfo.json',
       method: 'POST',
       agent: false,
       headers: {
@@ -36,10 +37,11 @@ module.exports = {
       res.on('data', (chunk) => {
         body += chunk;
       }).on('end', (chunk) => {
+        console.log(body);
         clearTimeout(responseTimer);
         if (res.statusCode == 200) {
-            let resp = JSON.parse(body);
-            response.send(JSON.parse(resp.resText));
+          let resp = JSON.parse(body);
+          response.send(JSON.parse(resp.resText));
         }
       });
     });
@@ -58,8 +60,9 @@ module.exports = {
     });
 
     var options = {
-      hostname: 'api.ppmiao.com',
-      path: '/user/getExchangeList.json',
+      hostname: '121.40.211.34',
+      port: 8089,
+      path: '/stone-rest/rest/user/getExchangeList.json',
       method: 'POST',
       agent: false,
       headers: {
@@ -79,8 +82,8 @@ module.exports = {
       }).on('end', (chunk) => {
         clearTimeout(responseTimer);
         if (res.statusCode == 200) {
-            let resp = JSON.parse(body);
-            response.send(JSON.parse(resp.resText));
+          let resp = JSON.parse(body);
+          response.send(JSON.parse(resp.resText));
         }
       });
     });
@@ -100,8 +103,9 @@ module.exports = {
     });
 
     var options = {
-      hostname: 'api.ppmiao.com',
-      path: '/user/claimExchange.json',
+      hostname: '121.40.211.34',
+      port: 8089,
+      path: '/stone-rest/rest/user/claimExchange.json',
       method: 'POST',
       agent: false,
       headers: {
@@ -121,8 +125,8 @@ module.exports = {
       }).on('end', (chunk) => {
         clearTimeout(responseTimer);
         if (res.statusCode == 200) {
-            let resp = JSON.parse(body);
-            response.send(JSON.parse(resp.resText));
+          let resp = JSON.parse(body);
+          response.send(JSON.parse(resp.resText));
         }
       });
     });
@@ -134,100 +138,102 @@ module.exports = {
     });
     req.write(data);
     req.end();
-},
-getWeeklyAward: function(request, response, callback) {
-  var data = querystring.stringify({
-    token: this.des(request.body.token)
-  });
+  },
+  getWeeklyAward: function(request, response, callback) {
+    var data = querystring.stringify({
+      token: this.des(request.body.token)
+    });
 
-  var options = {
-    hostname: 'api.ppmiao.com',
-    path: '/user/getWeeklyAward.json',
-    method: 'POST',
-    agent: false,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': data.length,
-    }
-  };
-  let body = '';
-  var req = http.request(options, (res) => {
-    var responseTimer = setTimeout(function() {
-      res.destroy();
-      debug('......Response Timeout......');
-    }, 5000);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-      body += chunk;
-    }).on('end', (chunk) => {
-      clearTimeout(responseTimer);
-      if (res.statusCode == 200) {
+    var options = {
+      hostname: '121.40.211.34',
+      port: 8089,
+      path: '/stone-rest/rest/user/getWeeklyAward.json',
+      method: 'POST',
+      agent: false,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': data.length,
+      }
+    };
+    let body = '';
+    var req = http.request(options, (res) => {
+      var responseTimer = setTimeout(function() {
+        res.destroy();
+        debug('......Response Timeout......');
+      }, 5000);
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        body += chunk;
+      }).on('end', (chunk) => {
+        clearTimeout(responseTimer);
+        if (res.statusCode == 200) {
           let resp = JSON.parse(body);
           response.send(JSON.parse(resp.resText));
-      }
+        }
+      });
     });
-  });
-  req.on('error', function(e) {
-    if (callback) {
-      callback(e, null);
-    }
-    console.log('problem with request: ' + e.message);
-  });
-  req.write(data);
-  req.end();
-},
-claimWeeklyAward: function(request, response, callback) {
-  var data = querystring.stringify({
-    token: this.des(request.body.token),
-    id: request.body.id
-  });
+    req.on('error', function(e) {
+      if (callback) {
+        callback(e, null);
+      }
+      console.log('problem with request: ' + e.message);
+    });
+    req.write(data);
+    req.end();
+  },
+  claimWeeklyAward: function(request, response, callback) {
+    var data = querystring.stringify({
+      token: this.des(request.body.token),
+      id: request.body.id
+    });
 
-  var options = {
-    hostname: 'api.ppmiao.com',
-    path: '/user/claimWeeklyAward.json',
-    method: 'POST',
-    agent: false,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': data.length,
-    }
-  };
-  let body = '';
-  var req = http.request(options, (res) => {
-    var responseTimer = setTimeout(function() {
-      res.destroy();
-      debug('......Response Timeout......');
-    }, 5000);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-      body += chunk;
-    }).on('end', (chunk) => {
-      clearTimeout(responseTimer);
-      if (res.statusCode == 200) {
+    var options = {
+      hostname: '121.40.211.34',
+      port: 8089,
+      path: '/stone-rest/rest/user/claimWeeklyAward.json',
+      method: 'POST',
+      agent: false,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': data.length,
+      }
+    };
+    let body = '';
+    var req = http.request(options, (res) => {
+      var responseTimer = setTimeout(function() {
+        res.destroy();
+        debug('......Response Timeout......');
+      }, 5000);
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        body += chunk;
+      }).on('end', (chunk) => {
+        clearTimeout(responseTimer);
+        if (res.statusCode == 200) {
           let resp = JSON.parse(body);
           response.send(JSON.parse(resp.resText));
-      }
+        }
+      });
     });
-  });
-  req.on('error', function(e) {
-    if (callback) {
-      callback(e, null);
-    }
-    console.log('problem with request: ' + e.message);
-  });
-  req.write(data);
-  req.end();
-},
-des: function(token) {
-  var key = '5Df8$&@S';
-  var iv = CryptoJS.enc.Utf8.parse(key);
-  var key = CryptoJS.enc.Utf8.parse(key);
-  var decrypted = CryptoJS.TripleDES.decrypt(token, key, {
-    iv: iv,
-    mode: CryptoJS.mode.CBC,
-    padding: CryptoJS.pad.Pkcs7
-  });
-  // 转换为 utf8 字符串
-  decrypted = CryptoJS.enc.Utf8.stringify(decrypted);
-}
+    req.on('error', function(e) {
+      if (callback) {
+        callback(e, null);
+      }
+      console.log('problem with request: ' + e.message);
+    });
+    req.write(data);
+    req.end();
+  },
+  des: function(token) {
+    var key = '5Df8$&@S';
+    var iv = CryptoJS.enc.Utf8.parse(key);
+    var key = CryptoJS.enc.Utf8.parse(key);
+    var decrypted = CryptoJS.TripleDES.decrypt(token, key, {
+      iv: iv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7
+    });
+    // 转换为 utf8 字符串
+    decrypted = CryptoJS.enc.Utf8.stringify(decrypted);
+  }
 };
