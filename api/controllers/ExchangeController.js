71627edc4a96fcb5a909341e94983b21341c7c58
liuchenxi <lcx010222@163.com>
2017-06-24 15:26:08 +0800

    调通的解密

--- api/controllers/ExchangeController.js
+++ api/controllers/ExchangeController.js
@@ -8,6 +8,7 @@
 var http = require('http');
 var CryptoJS = require("crypto-js");
 var querystring = require('querystring');
+var loda = require('lodash');
 var key = '5Df8$&@S';
 var iv = CryptoJS.enc.Utf8.parse(key);
 var key = CryptoJS.enc.Utf8.parse(key);
@@ -33,44 +34,44 @@ module.exports = {
         debug('......Response Timeout......');
       }, 5000);
       res.setEncoding('utf8');
+    //   loda.replace()
       res.on('data', (chunk) => {
         body += chunk;
       }).on('end', (chunk) => {
         clearTimeout(responseTimer);
         if (res.statusCode == 200) {
           let resp = JSON.parse(body);
-          var key = '5Df8$&@S';
-          var iv = CryptoJS.enc.Utf8.parse(key);
-          var key = CryptoJS.enc.Utf8.parse(key);
-          var decrypted = CryptoJS.TripleDES.decrypt({
-              ciphertext: CryptoJS.enc.Base64.parse(resp.resText)
-          }, key, {
+          console.log(resp.resText);
+          var data1 = '{"code":"0","result":"1kp0P/DuJGAdgc9jl0QoGDjHH9/azHSPcXUTkXZ8CacdkoKC4WHzt312jVihC7HL","errorType":null,"errorMsg":"正常","success":true}';
+          var data2 = 'eyqHDoeDrIra8sRTOHhugsPus7Yb6N7FKpi5uiKKE1Wy8lesZPflSqsf/YP2e8XWxQrZ/T5cbyV4\n0FlETnrJLp3Y0snHyEzYknri7qSmoXVQanPfupNyYP5Zp5WfuJ79bpdQqwI8lXtRUpIRfPp8aoaL\ndSmDAtRKOTrCnjPG6FwgmAAQ3L9E6/jUV6t2Ar6S\n';
+          var data3 = 'eyqHDoeDrIra8sRTOHhugsPus7Yb6N7FKpi5uiKKE1Wy8lesZPflSqsf/YP2e8XWxQrZ/T5cbyV40FlETnrJLp3Y0snHyEzYknri7qSmoXVQanPfupNyYP5Zp5WfuJ79bpdQqwI8lXtRUpIRfPp8aoaLdSmDAtRKOTrCnjPG6FwgmAAQ3L9E6/jUV6t2Ar6S'
+        //   if(data2.replace(/[\r\n]/g, "")){
+        //       console.log(data2);
+        //   }
+          data2 = data2.replace(/[\r\n]/g, "")
+          console.log(data2);
+          console.log(data3);
+          // Encrypt
+          //   var ciphertext = CryptoJS.DES.encrypt(JSON.stringify(data1), '5Df8$&@S').toString();
+          var ciphertext = CryptoJS.DES.encrypt(resp.resText, key, {
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7,
+            blockSize: 8
+          });
+        //   console.log('ciphertext');
+        //   console.log(ciphertext.toString());
+
+          // Decrypt
+          var ciphertext = CryptoJS.DES.decrypt(data2, key, {
             iv: iv,
             mode: CryptoJS.mode.CBC,
             padding: CryptoJS.pad.Pkcs7
           });
-          console.log(decrypted);
-          console.log("------------解码后-----------------");
-          var decrypt = decrypted.toString(CryptoJS.enc.Utf8);
-          console.log(decrypt);
-
-          var data = [{
-            id: 1
-          }, {
-            id: 2
-          }]
-          // Encrypt
-          var ciphertext = CryptoJS.TripleDES.encrypt(JSON.stringify(data), 'secret key 123');
-
-          // Decrypt
-          var bytes = CryptoJS.TripleDES.decrypt(ciphertext.toString(), 'secret key 123');
-          console.log(bytes);
-          var decryptedData = bytes.toString(CryptoJS.enc.Utf8);
-          console.log(decryptedData);
-
-
-
-          response.send(decrypt);
+          ciphertext = CryptoJS.enc.Utf8.stringify(ciphertext);
+          //   var decryptedData = ciphertext.toString(CryptoJS.enc.Utf8);
+          console.log(ciphertext);
+          //   response.send(decrypt);
         }
       });
     });
