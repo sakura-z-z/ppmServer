92fcfbd80302ec3c229b53fc092c77bc48dc0328
liuchenxi <lcx010222@163.com>
2017-06-30 14:48:22 +0800

    123

--- api/controllers/BindCardController.js
+++ api/controllers/BindCardController.js
@@ -6,10 +6,122 @@
  */
 var querystring = require('querystring');
 module.exports = {
+  // getBanklist: function(request, response, callback) {
+  //   var data = querystring.stringify({
+  //     token: request.body.token
+  //   });
+  //   GlobalMethods.httpPost(request, response, callback, GlobalVal.apiHost, '/user/getAllBindCardInfo.json', data);
+  // },
+  // updateMainBankCard: function(request, response, callback) {
+  //   var data = querystring.stringify({
+  //     token: request.body.token
+  //   });
+  //   GlobalMethods.httpPost(request, response, callback, GlobalVal.apiHost, '/user/getAllBindCardInfo.json', data);
+  // },
   getBanklist: function(request, response, callback) {
-    var data = querystring.stringify({
-      token: request.body.token
+    if (request.body.token != undefined) {
+      var data = querystring.stringify({
+        token: request.body.token
+      });
+    } else {
+      var data = querystring.stringify({});
+    }
+
+    var options = {
+      hostname: '114.55.85.42',
+      port: 10502,
+      //   hostname: 'api.ppmiao.com',
+      path: '/stone-rest/rest/user/getAllBindCardInfo.json',
+      method: 'POST',
+      agent: false,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': data.length,
+      }
+    };
+    let body = '';
+    var req = http.request(options, (res) => {
+      var responseTimer = setTimeout(function() {
+        res.destroy();
+        debug('......Response Timeout......');
+      }, 5000);
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        body += chunk;
+      }).on('end', (chunk) => {
+        clearTimeout(responseTimer);
+        if (res.statusCode == 200) {
+          console.log(body);
+          let resp = JSON.parse(body); 
+          if (resp.isEnc == 'Y') {
+            response.send(responseDesNormal(resp));
+          } else {
+            response.send(resp);
+          }
+        }
+      });
     });
-    GlobalMethods.httpPost(request, response, callback, GlobalVal.apiHost, '/user/getAllBindCardInfo.json', data);
+    req.on('error', function(e) {
+      if (callback) {
+        callback(e, null);
+      }
+      console.log('problem with request: ' + e.message);
+    });
+    req.write(data);
+    req.end();
   },
+  updateMainBankCard: function(request, response, callback) {
+      if (request.body.bankId != undefined && request.body.token != undefined) {
+        var data = querystring.stringify({
+          token: request.body.token,
+          bankId: request.body.bankId
+        });
+      } else {
+        var data = querystring.stringify({});
+      }
+
+    var options = {
+      hostname: '114.55.85.42',
+      port: 10502,
+      //   hostname: 'api.ppmiao.com',
+      path: '/stone-rest/rest/user/updateMainBankCard.json',
+      method: 'POST',
+      agent: false,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': data.length,
+      }
+    };
+    let body = '';
+    var req = http.request(options, (res) => {
+      var responseTimer = setTimeout(function() {
+        res.destroy();
+        debug('......Response Timeout......');
+      }, 5000);
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        body += chunk;
+      }).on('end', (chunk) => {
+        clearTimeout(responseTimer);
+        if (res.statusCode == 200) {
+          console.log(body);
+          let resp = JSON.parse(body); 
+          if (resp.isEnc == 'Y') {
+            response.send(responseDesNormal(resp));
+          } else {
+            response.send(resp);
+          }
+        }
+      });
+    });
+    req.on('error', function(e) {
+      if (callback) {
+        callback(e, null);
+      }
+      console.log('problem with request: ' + e.message);
+    });
+    req.write(data);
+    req.end();
+  }
+
 };
