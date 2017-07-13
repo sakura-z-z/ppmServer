6777150e619b7b07b8f6d62b872f2104c1f815be
liuchenxi <lcx010222@163.com>
2017-07-13 15:14:57 +0800

    123

--- api/controllers/CommonController.js
+++ api/controllers/CommonController.js
@@ -7,7 +7,7 @@
 
  module.exports = {
    getDesToken: function(request, response, callback) {
-       let body = "eyqHDoeDrIra8sRTOHhugud1AxGR9QqSMaN5t42i4C5ohjD6ianKaIc/iHAKsIDsYNBY+Ediq/xY\no5Or9gymwThyLG9l93xvf7MoWapnGiqo/bvSDEx6v0stwH5H6YLrCUNQ556r15L7j3x7oLWYxjjl\nBR0WBPfjCbxLBzZUTUq7QJrHD2Wjn9ZzieZuUv3zTeFXMXJ4VJ1hfhYzFVJAR8Eq9aa+EV40+PLj\nzRvN3BQvDupPOVe2fOnGCpzCZiqKT/MPl0N1wq5D95HGQDRFmU6YwPzXcNzr/uvPGv8+LykKCX3c\ncCklkG2eeWGXREU3r7+Nfceax/bdJg1p/7xrlee9VYuBxln4eu0seFK8/7xWIhcY7SHifvIny79S\nMikfAa0nNtj1NrtKmnc0epLsBaXk2qtwvd9aG9Y7HHbmZA8PhJN/kwz6K+RNmVsE/BrV/k+yDzmR\n91Td4AaU0OoPe1evIyEEWH4/LB50r0E1dArkUmbqC1UzsDe0UBP0NWEXgU3XSX/RItI=\n"
+       let body = "eyqHDoeDrIra8sRTOHhugud1AxGR9QqSrPhl22jdNnsaUr78NvaxzSQOsTt+WOrEzBVjf/Wztxwt\nabiRD+2nnPwWdDKv0JegQiUMJrDqkS2HbZekE3O7/IipJAAS9sipYpXsDkR/yq+1POTM08AzbUDb\n8nBxdPVjJznIH/gIeT4xBDB9JO1zbaOldwtmKIxW0q0/tNLniHjdmusyc/Ly8pg8psK+kbwmHkoy\nctMMTYHZwVwehs1hXNFNTkaOf+QUiTsUCi8mzOWkMJ6q44VsMFMtCiMWte7irGs+BQivKJFjZqor\nBFNYpcELzOgzhYMJfL6Uv1wmPoiertYwxxqstrAr0lFXB5lpZcXoL3yebRg6uON0yyf3JyYBVmqK\nmRku/IHsARl79wSyh8WeNUIOtS1eWK7PSk2mRQnm4ZiyyEYEcb/bEDSXbrUZvZdqsZgv/h6lXOmu\nzwJ9UQLlqHZ9Rv8fv96rfHFZK4xpf/thd52UVoQ7dhpQ43EDaKWkqjz1Rh7J0FnS7vhPBahbGWu+\nrPn4l5Wcql1UqMjRvQPRxRVVwAkLUJQOoSqQUHeRN5RbzFFeRbaZY/0lIY3HGsmVYibQAyW0H9RK\nu7pqJbjwD44=\n"
        resp = JSON.parse(GlobalMethods.ReleaseToken(body));
        let data = resp.result.token;
        console.log(GlobalMethods.ReleaseDesToken(data));
