9c0b1f3cee5ee385c23ed27e72b4539cb6c9eb13
liuchenxi <lcx010222@163.com>
2017-07-18 17:55:33 +0800

    update

--- api/controllers/UpvaluationController.js
+++ api/controllers/UpvaluationController.js
@@ -11,21 +11,23 @@
  module.exports = {
    checkRaiseSalaryInfo: function(request, response, callback) {
      var data = querystring.stringify({
-       token: GlobalMethods.tokenDes(request.body.token)
+       token: GlobalMethods.tokenDes(request.body.token),
+       versionName: request.body.versionName
      });
      GlobalMethods.httpPost(request, response, callback, GlobalVal.apiHost, '/message/checkRaiseSalaryInfo.json', data);
    },
    claimRaiseSalaryInterestCoupon: function(request, response, callback) {
      var data = querystring.stringify({
-			//  versionName: request.body.versionName,
-       token: GlobalMethods.tokenDes(request.body.token),
+	   versionName: request.body.versionName,
+       token: GlobalMethods.tokenDes(request.body.token)
 			//  id: request.body.id
      });
      GlobalMethods.httpPost(request, response, callback, GlobalVal.apiHost, '/message/claimRaiseSalaryInterestCoupon.json', data);
    },
 	 getUserRaiseSalaryInvestAward: function(request, response, callback) {
      var data = querystring.stringify({
-       token: GlobalMethods.tokenDes(request.body.token)
+       token: GlobalMethods.tokenDes(request.body.token),
+       versionName: request.body.versionName,
      });
      GlobalMethods.httpPost(request, response, callback, GlobalVal.apiHost, '/message/getUserRaiseSalaryInvestAward.json', data);
    }
