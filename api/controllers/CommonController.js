a5b7d3b6f03f81728a2a7d0d78fded3f6ca31260
liuchenxi <lcx010222@163.com>
2017-07-24 14:30:29 +0800

    get token

--- api/controllers/CommonController.js
+++ api/controllers/CommonController.js
@@ -7,7 +7,7 @@
 
  module.exports = {
    getDesToken: function(request, response, callback) {
-       let body = "eyqHDoeDrIra8sRTOHhugud1AxGR9QqSu41v2mrJiU2iQcusDIgW49hD2+f352rlAJ+NkTCdurBY\nOEmqKfrxyvnzqy4o2fIZ9ckdJz3zGu1/agER4wihmlO20/1tQ5rnAi+Ee1hCN1oPLpyb+Tkl7mVd\nt0jjHkkRLAc89BIvFkKGm3xIlQL3bs7fNiJLVtBKEdII4dvBfpY8p+PgBIRrpG86pZe2QLrtOsA6\nPM8ykuOCMdknYMJXVADu1S76N2n00DtR30JY6bn6VMzTYawbl56OAbXz8k77p+yRLBd2fBNDfI88\nLYUZWoCW+qixx1cUZ1evGyEZwbzN7/rKFB27Zh59zLfVyNtpZoU+VgwBLc/8Jp/pSJH7Qbu3Y13R\nTnOWApDVFBbhqaOhD/0uNttTa/f+aEWHFSefqCXy+Yc8fU/YcckzCdXcDy8GVqG9rg3j+35EoQTg\nptFDXVfa0crND9F/+JWG9p29yN3LYOzBJKJtIBj1bp4l9GK14xDOtkalpEUa71nCwHITgQB+yDR2\njGNbtVE04YNe8hlXELnbJBjinShTB7sk6g==\n"
+       let body = "eyqHDoeDrIra8sRTOHhugud1AxGR9QqSpF/jAuqpco1dlMHN2r1GRjA9kiR/SUzNnR6slJJOnoO7\nfxh25Pjn5wl2R7XdwvJHcjtyIrfCu/NWMgk3KGOCVmq6YKKvnyfru2KcCHKo0Iug8J8KBn4J/Pmy\nk0SxfqohSqIEVrzRdfppBaaaZ72ID9liM95aw3KYA4S38VHcBE/X6+ZkqtC99Qsn6NEfSe7oWnXC\nYZbMyld8/srAND2Mv/P804aVTz6XwnPB0Sc41h2wQi2Rxvh/FopcND/Fbg29efhSQUfMXKu+NK34\n32ct4pTNb34Ac07CQp08ydisi0FUDdW4SpHa5nGC21F46PHcGjZe/elFYFar0WjmjB4thfaBslc+\nYfzgOxYA1Elc3vXtItKKK3GPydLqthKMTT/HHS24itx77Lr85Qd8R6adA5LJJLDuoiUE7gBrAHyU\n06XvrNmL16Khh9MbTYbTwikGK2dRTo7confpDMXQe28RfGScswV1JPjdzP0pqVjJigDzsvz7pRa2\nWVqDd9potOXwVG43pU9edqhcrtEBCuA/gzJ9uqtQMolzzJyEhzCnPmVxfV4udv9u52iqvSYOksdW\nPfDGdkZB/AI=\n"
        resp = JSON.parse(GlobalMethods.ReleaseToken(body));
        let data = resp.result.token;
        console.log(GlobalMethods.ReleaseDesToken(data));
@@ -15,5 +15,9 @@
    getPhone: function(request, response, callback) {
        let body = "eyqHDoeDrIra8sRTOHhugrMYs+9jbMm7+/OoVJmwhaVZ3BNEmmB/MfhGoeLNIaBnQAQn10BdNtSm\nxtuP8OiITODeDYFyAhEuJhr3H8V7zxQJdxA+aD+1Ag==\n"
        console.log(GlobalMethods.ReleaseToken(body));
+   },
+   getToken: function(request, response, callback) {
+       let body = "VEtfMjAxNzA2MDgxNzM2MDVfNjYyOTRfMTM2Njk5"
+       console.log(GlobalMethods.ReleaseDesToken(body));
    },
  };
