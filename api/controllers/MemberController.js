782e8e5820d134f59e16d5412d055b41c53cf5dc
liuchenxi <lcx010222@163.com>
2017-08-08 11:13:42 +0800

    地址

--- api/controllers/MemberController.js
+++ api/controllers/MemberController.js
@@ -10,7 +10,8 @@ var querystring = require('querystring');
 var mysql = require('mysql');
 var moment = require('moment');
 var connection = mysql.createConnection({
-  host: 'rm-uf6s86ucfa1mvy1m8o.mysql.rds.aliyuncs.com',
+  // host: 'rm-uf6s86ucfa1mvy1m8o.mysql.rds.aliyuncs.com',
+   host: 'rm-uf6s86ucfa1mvy1m8.mysql.rds.aliyuncs.com',
   user: 'pptang_123',
   password: 'E8b9J7TjPs0u4Nf',
   port: '3306',
