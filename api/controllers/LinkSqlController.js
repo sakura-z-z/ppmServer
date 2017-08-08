e5c9127ca211bbf3255b4de14447e5a892fb469a
liuchenxi <lcx010222@163.com>
2017-08-08 11:22:54 +0800

    session

--- api/controllers/LinkSqlController.js
+++ api/controllers/LinkSqlController.js
@@ -26,7 +26,8 @@ module.exports = {
     let startTime = "'2017-07-02 00:00:00'";
     const TABLE = "s_user_due_detail";
     let connection = mysql.createConnection({
-      host: 'rm-uf6s86ucfa1mvy1m8o.mysql.rds.aliyuncs.com',
+    //   host: 'rm-uf6s86ucfa1mvy1m8o.mysql.rds.aliyuncs.com',
+      host: 'rm-uf6s86ucfa1mvy1m8.mysql.rds.aliyuncs.com',
       user: 'pptang_123',
       password: 'E8b9J7TjPs0u4Nf',
       port: '3306',
