8d03092525d62615d302cdf43f3aaf083bdd36db
by <forestby@163.com>
2017-08-21 16:45:43 +0800

    juejin1

--- api/controllers/LinkSqlController.js
+++ api/controllers/LinkSqlController.js
@@ -6,7 +6,6 @@
  */
 var querystring = require('querystring');
 var async = require('async');
-var mysql = require("mysql"); //调用MySQL模块
 var query = require('../models/pool');
 module.exports = {
     tokenExpire: function(request, response, callback) {
@@ -24,22 +23,18 @@ module.exports = {
 		// 	return false;
 		// }
 		// let userId = GlobalMethods.getUser(request, response, callback).id;
-		userId = 66340;
-		let startTime = "'2017-07-02 00:00:00'";
+		let userId = 66340;
+		let startTime = "'2017-07-12 00:00:00'";
 
-		// const mysql = require("mysql"); //调用MySQL模块
 		const TABLE = "s_user_due_detail";
-		let connection = mysql.createConnection({host: 'rm-uf6s86ucfa1mvy1m8o.mysql.rds.aliyuncs.com', user: 'pptang_123', password: 'E8b9J7TjPs0u4Nf', port: '3306' ,database: 'ppmiao_test'});
-		connection.connect();
-		connection.query("select sum(due_capital) from " + TABLE + " where user_id = " + userId + " and start_time > " + startTime, function(error, results, fields) {
+		query("use ppmiao_test;select sum(due_capital) from " + TABLE + " where user_id = " + userId + " and start_time > " + startTime, function(error, results, fields) {
 			if (error) {
 				throw error;
 			}
 			if (results) {
-				response.send({dueCapital: results[0]['sum(due_capital)']});
+                console.log(results);
+				response.send({dueCapital: results[1][0]['sum(due_capital)']});
 			}
 		});
-		connection.end();
-		// return userId;
 	}
 };
