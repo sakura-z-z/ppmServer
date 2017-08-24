4c8b5ecd7364909f0e9eeeb93539d02832d60b87
liuchenxi <lcx010222@163.com>
2017-08-24 17:14:28 +0800

    dev

--- api/controllers/LoverController.js
+++ api/controllers/LoverController.js
@@ -11,10 +11,9 @@ var querystring = require('querystring');
 var actHost = 'http://testing.ppmiao.com';
 // var actHost = '121.40.211.34';
 var actId = 17;
-var dev = undefined;
 module.exports = {
   actInfo: function(request, response, callback) {
-    if (dev != undefined) {
+    if (request.body.dev != undefined) {
       var data = querystring.stringify({
         token: request.body.token,
         lotteryId: actId,
@@ -30,7 +29,7 @@ module.exports = {
     GlobalMethods.httpPostPHP(request, response, callback, actHost, '/index.php?c=lottery&a=get_lottery_info', data, 80);
   },
   drawResult: function(request, response, callback) {
-    if (dev != undefined) {
+    if (request.body.dev != undefined) {
       var data = querystring.stringify({
         token: request.body.token,
         lotteryId: actId,
@@ -46,7 +45,7 @@ module.exports = {
     GlobalMethods.httpPostPHP(request, response, callback, actHost, '/index.php?c=lottery&a=do_lottery', data, 80);
   },
   winningList: function(request, response, callback) {
-    if (dev != undefined) {
+    if (request.body.dev != undefined) {
       var data = querystring.stringify({
         token: request.body.token,
         lotteryId: actId,
@@ -65,7 +64,7 @@ module.exports = {
     GlobalMethods.httpPostPHP(request, response, callback, actHost, '/index.php?c=lottery&a=get_user_lottery_log', data, 80);
   },
   drawTimes: function(request, response, callback) {
-    if (dev != undefined) {
+    if (request.body.dev != undefined) {
       var data = querystring.stringify({
         token: request.body.token,
         lotteryId: actId,
