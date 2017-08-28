0e60b54dc789615a9543d317c9eee81496d8d042
liuchenxi <lcx010222@163.com>
2017-08-28 13:42:22 +0800

    1

--- api/controllers/ValentineController.js
+++ api/controllers/ValentineController.js
@@ -11,12 +11,12 @@ module.exports = {
 		var data = querystring.stringify({
 			keyName: request.body.keyName
 		});
-		GlobalMethods.httpPost(request, response, callback,GlobalVal.apiHost, '/ppmiao-rest/payment/activity/inviteFriend/getAwards.htm', data);
+		GlobalMethods.httpPost(request, response, callback,GlobalVal.apiHost, '/payment/activity/inviteFriend/getAwards.htm', data);
 	},
 	getMagpieProjects: function(request, response, callback) {
 		var data = querystring.stringify({
 			title: request.body.title
 		});
-		GlobalMethods.httpPost(request, response, callback,GlobalVal.apiHost, '/ppmiao-rest/payment/activity/inviteFriend/getMagpieProjects.htm', data);
+		GlobalMethods.httpPost(request, response, callback,GlobalVal.apiHost, '/payment/activity/inviteFriend/getMagpieProjects.htm', data);
 	},
 };
