e46ea842eeb88df10ae3f6cf5f8b03dec0d25c13
liuchenxi <lcx010222@163.com>
2017-08-28 13:40:44 +0800

    update

--- api/controllers/ValentineController.js
+++ api/controllers/ValentineController.js
@@ -11,12 +11,12 @@ module.exports = {
 		var data = querystring.stringify({
 			keyName: request.body.keyName
 		});
-		GlobalMethods.httpPost(request, response, callback,GlobalVal.apiHost, '/ppmiao-rest/payment/activity/inviteFriend/getAwards.htm', data, GlobalVal.apiPort);
+		GlobalMethods.httpPost(request, response, callback,GlobalVal.apiHost, '/ppmiao-rest/payment/activity/inviteFriend/getAwards.htm', data);
 	},
 	getMagpieProjects: function(request, response, callback) {
 		var data = querystring.stringify({
 			title: request.body.title
 		});
-		GlobalMethods.httpPost(request, response, callback,GlobalVal.apiHost, '/ppmiao-rest/payment/activity/inviteFriend/getMagpieProjects.htm', data, GlobalVal.apiPort);
+		GlobalMethods.httpPost(request, response, callback,GlobalVal.apiHost, '/ppmiao-rest/payment/activity/inviteFriend/getMagpieProjects.htm', data);
 	},
 };
--- api/services/GlobalVal.js
+++ api/services/GlobalVal.js
@@ -2,7 +2,7 @@ if (sails.config.environment === 'development') {
     var apiHost='api.test.ppmiao.com';
 }
 if (sails.config.environment === 'production') {
-    var apiHost='120.55.240.160';
+    var apiHost='api.ppmiao.cn';
     // http://120.55.240.160:8089/ppmiao-rest/payment/activity/inviteFriend/checkActivity.htm
 }
 module.exports = {
