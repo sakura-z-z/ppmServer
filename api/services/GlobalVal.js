4069c0b5c26567b7cdcd0d085e476359be0f0a97
liuchenxi <lcx010222@163.com>
2017-09-06 17:50:03 +0800

    123

--- api/services/GlobalVal.js
+++ api/services/GlobalVal.js
@@ -2,8 +2,8 @@ if (sails.config.environment === 'development') {
     var apiHost='api.test.ppmiao.com';
 }
 if (sails.config.environment === 'production') {
-    // var apiHost='api.ppmiao.cn';
-    var apiHost='api.prepare.ppmiao.cn';
+    var apiHost='api.ppmiao.cn';
+    // var apiHost='api.prepare.ppmiao.cn';
 
     // http://120.55.240.160:8089/ppmiao-rest/payment/activity/inviteFriend/checkActivity.htm
 }
