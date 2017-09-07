858ea2fe643c7587d45d491fb971fe684aab1ca5
liuchenxi <lcx010222@163.com>
2017-09-07 11:34:50 +0800

    redis

--- api/services/GlobalMethods2.js
+++ api/services/GlobalMethods2.js
@@ -6,35 +6,31 @@ var key = '5Df8$&@S';
 var iv = CryptoJS.enc.Utf8.parse(key);
 var key = CryptoJS.enc.Utf8.parse(key);
 var query = require('../models/pool');
-var redis = require('redis'),
-    RDS_PORT = 6379,        //端口号
-    RDS_HOST = 'r-uf678e1e6a9deeb4.redis.rds.aliyuncs.com',    //服务器IP
-    RDS_PWD = 'Aa311512',
-    RDS_OPTS = {},            //设置项
-    client = redis.createClient(RDS_PORT,RDS_HOST,RDS_OPTS);
-    client.on("error", function(error) {
-    console.log(error);
-});
-    client.auth(RDS_PWD,function(){
-    console.log('通过认证');
-});
-
-client.on('ready',function(res){
-    console.log('ready');
-});
-
+// var redis = require('redis'),
+//     RDS_PORT = 6379,        //端口号
+//     RDS_HOST = 'r-uf678e1e6a9deeb4.redis.rds.aliyuncs.com',    //服务器IP
+//     RDS_PWD = 'Aa311512',
+//     RDS_OPTS = {},            //设置项
+//     client = redis.createClient(RDS_PORT,RDS_HOST,RDS_OPTS);
 
 module.exports = {
-    connectRedis: function(){
-        console.log('开始调用');
-        client.on("error", function(error) {
-    console.log(error);
-});
-        client.auth(RDS_PWD,function(){
-        console.log('通过认证');
+  connectRedis: function() {
+      var redis = require('redis'),
+          RDS_PORT = 6379,        //端口号
+          RDS_HOST = 'r-uf678e1e6a9deeb4.redis.rds.aliyuncs.com',    //服务器IP
+          RDS_PWD = 'Aa311512',
+          RDS_OPTS = {},            //设置项
+          client = redis.createClient(RDS_PORT,RDS_HOST,RDS_OPTS);
+    client.on('ready', function(res) {
+      console.log('ready');
     });
-    client.on('ready',function(res){
-        console.log('ready');
+    client.on('connect', function() {
+      client.set('author', 'lcx', redis.print);
+      client.get('ppmiao_uid_66674', redis.print);
+      console.log('connect');
     });
-    }
+    client.on("error", function(error) {
+      console.log(error);
+    });
+  }
 };
