8aa8bbbf6256a175d7375a35775f5aae879181df
liuchenxi <lcx010222@163.com>
2017-09-07 11:39:19 +0800

    update

--- api/services/GlobalMethods2.js
+++ api/services/GlobalMethods2.js
@@ -15,12 +15,15 @@ var query = require('../models/pool');
 
 module.exports = {
   connectRedis: function() {
-      var redis = require('redis'),
-          RDS_PORT = 6379,        //端口号
-          RDS_HOST = 'r-uf678e1e6a9deeb4.redis.rds.aliyuncs.com',    //服务器IP
-          RDS_PWD = 'Aa311512',
-          RDS_OPTS = {},            //设置项
-          client = redis.createClient(RDS_PORT,RDS_HOST,RDS_OPTS);
+    var redis = require('redis'),
+      RDS_PORT = 6379, //端口号
+      RDS_HOST = 'r-uf678e1e6a9deeb4.redis.rds.aliyuncs.com', //服务器IP
+      RDS_PWD = 'Aa311512',
+      RDS_OPTS = {}, //设置项
+      client = redis.createClient(RDS_PORT, RDS_HOST, RDS_OPTS);
+    client.auth(RDS_PWD, function() {
+      console.log('通过认证');
+    });
     client.on('ready', function(res) {
       console.log('ready');
     });
