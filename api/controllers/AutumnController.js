b310d8f00b68c995dd22b06b639150bfdcdbc6e4
liuchenxi <lcx010222@163.com>
2017-09-13 17:33:36 +0800

    减一天

--- api/controllers/AutumnController.js
+++ api/controllers/AutumnController.js
@@ -52,8 +52,8 @@ module.exports = {
             return;
           }
           if (rows[0] != undefined) {
-            startTime = moment(rows[0].start_time * 1000).add(1, 'days').format('YYYY-MM-DD HH:mm:ss');
-            endTime = moment(rows[0].end_time * 1000).add(1, 'days').format('YYYY-MM-DD HH:mm:ss');
+            startTime = moment(rows[0].start_time * 1000).format('YYYY-MM-DD HH:mm:ss');
+            endTime = moment(rows[0].end_time * 1000).format('YYYY-MM-DD HH:mm:ss');
             let sqlHead = "select total,user_id,case when p.total>=10000 and p.total<30000 then '40元现金券' when p.total >= 30000 and p.total < 50000 then '120元现金券' when p.total >= 50000 and p.total < 100000 then CONCAT(floor(p.total * 5 / 1000), '元现金券') when p.total >= 100000 and p.total < 300000 then CONCAT(floor(p.total * 5.5 / 1000), '元现金券') when p.total >= 300000 and p.total < 500000 then '小米 Pad3' when p.total >= 500000 and p.total < 800000 then 'iPad +罗技蓝牙键盘' when p.total >= 800000 and p.total < 1200000 then 'iPhone7' when p.total >= 1200000 then 'MacBook Air' else null end award from(SELECT SUM(s.due_capital)AS total, s.user_id FROM ";
             let sqlFoot = ' AND p.is_delete = 0 GROUP BY s.user_id ORDER BY sum(s.due_capital)DESC)p'
             let sql = sqlHead + userDB + ".s_project p, " + userDB + ".s_user_due_detail s WHERE p.id = s.project_id and p.title like '" + signName + "' AND s.add_time >= '" + startTime + "' AND s.add_time < '" + endTime + "'  AND s.user_id  =  " + userInfo.id + sqlFoot + ";";
