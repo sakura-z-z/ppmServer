1e21be568a869fcc5dcf91b504fc5f1840c25200
张昭 <traveler@Sakura.local>
2017-11-30 15:07:01 +0800

    bill

--- api/controllers/SinglesDayController.js
+++ api/controllers/SinglesDayController.js
@@ -8,8 +8,11 @@
 var querystring = require('querystring');
 module.exports = {
 	getActivityInvestLog: function(request, response, callback) {
-       var data = querystring.stringify({ keyName: request.body.keyName,token: GlobalMethods.tokenDes(request.body.token), isNew: request.body.isNew});
-       console.log(GlobalVal.apiHost);
+       var data = querystring.stringify({
+				 keyName: request.body.keyName,
+				 token: GlobalMethods.tokenDes(request.body.token),
+				 isNew: request.body.isNew
+			 });
        GlobalMethods.httpPost(request, response, callback, GlobalVal.apiHost, '/activity/getActivityInvestLog.json', data);
      }
 };
